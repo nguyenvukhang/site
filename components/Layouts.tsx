7c41c84d724e3ebbb15d8db7f4d0f38ff5deffbd
nguyenvukhang <brew4k@gmail.com>
2022-12-19 15:42:19 +0800

    feat: format code blocks

--- components/Layouts.tsx
+++ components/Layouts.tsx
@@ -1,6 +1,16 @@
 import type { NextRouter } from 'next/router'
 import { Header } from '../components/Header'
 
+const VerticalContent = (props: JSX.IntrinsicElements['div']) => (
+  <div className="flex flex-col max-w-full sm:max-w-2xl px-2 sm:px-5 md:px-10">{props.children}</div>
+)
+
+const HorizontalMiddle = (props: JSX.IntrinsicElements['div']) => (
+  <div className="flex justify-center w-full">
+    <VerticalContent>{props.children}</VerticalContent>
+  </div>
+)
+
 /**
  * classic <div> tag props + a NextRouter
  */
@@ -8,10 +18,12 @@ type LayoutProps = JSX.IntrinsicElements['div'] & { router: NextRouter }
 
 export const BlogPostLayout = (props: LayoutProps) => {
   return (
-    <div>
-      <Header router={props.router} />
-      {props.children}
-    </div>
+    <>
+      <HorizontalMiddle>
+        <Header router={props.router} />
+        <div className="overflow-x-hidden">{props.children}</div>
+      </HorizontalMiddle>
+    </>
   )
 }
 
