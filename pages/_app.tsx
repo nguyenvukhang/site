cfafb7e4d2baa31e6374289caa0c92e690a8cd0c
nguyenvukhang <brew4k@gmail.com>
2022-12-19 14:44:14 +0800

    this might be the future

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  console.log(router.route)
+
+  if (router.route === '/meme') {
+    return (
+      <div style={{ backgroundColor: 'red', margin: '100px' }}>
+        <Component {...pageProps} />
+      </div>
+    )
+  }
   return <Component {...pageProps} />
 }
