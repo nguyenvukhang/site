9fb14c8088a8309769b669977bcfb5b0d1fe2a39
nguyenvukhang <brew4k@gmail.com>
2022-12-21 17:16:52 +0800

    feat: single line header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,10 +16,9 @@ export const Header = (props: { router: NextRouter }) => {
   const { router } = props
   // root page header
   const RootHeader = () => (
-    <div className="flex flex-col mt-12 mb-10">
-      <TelescopeIcon size={32} className="fill-gray-400" />
-      <div className="h-2" />
-      <Name className="text-[1.2em] text-gray-900" />
+    <div className="flex flex-row mt-12 mb-10">
+      <TelescopeIcon size={32} className="fill-gray-400 mr-4" />
+      <Name className="text-[1.2em] text-gray-700 font-medium" />
     </div>
   )
   // non-root header (has a back button to navigate home)
