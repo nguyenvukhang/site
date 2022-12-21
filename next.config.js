4916ef210542c2e6d1c2d8abe9b82748ea65f9ce
nguyenvukhang <brew4k@gmail.com>
2022-12-21 23:48:57 +0800

    feat: autolink heading in-house

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const parseFrontmatter = require('./lib/scripts/frontmatter')
+const autolinkHeadings = require('./lib/scripts/autolink-headings')
 const linkPosts = require('./lib/scripts/link-posts')
 
 linkPosts(process.cwd())
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx$/,
-  options: { remarkPlugins: [parseFrontmatter] },
+  options: {
+    remarkPlugins: [parseFrontmatter],
+    rehypePlugins: [autolinkHeadings],
+  },
 })
 
 module.exports = withMDX({
