f80c49dc5d39df1da87cbe96996c2e78c41218dc
nguyenvukhang <brew4k@gmail.com>
2023-06-27 18:27:22 +0800

    feat: add anchor id generator

---
+++ astro/src/utils.ts
@@ -0,0 +1,6 @@
+// this file must NOT depend on `fs` nor `path` in order to be usable
+// everywhere
+
+export function toAnchorId(text: string, prefix = ''): string {
+  return prefix +  text.toLowerCase().replace(/\s/g, '-')
+}
