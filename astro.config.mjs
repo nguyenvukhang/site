e35b5a4990152e9481a7e8b88f182bcf783321cf
nguyenvukhang <brew4k@gmail.com>
2026-02-15 10:43:37 +0800

    Stabilize date somewhat

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,11 @@ const ARTICLES_CONTENT_DIR = 'content/articles'
 
 function mdxDatePlugin() {
   return function (_, file) {
-    /** @param {Date} d */
-    const valid = (d) => d instanceof Date && !isNaN(d)
-
     /** @type string */
-    const path = file.path
-
-    if (path.includes(ARTICLES_CONTENT_DIR)) {
-      const filenameDate = new Date(file.stem.split('-').slice(0, 3).join('-'))
-      const metaDate = new Date(
-        Date.parse(`${file.data.astro.frontmatter.pubDate} 00:00:00 GMT`),
-      )
-      if (!valid(filenameDate)) {
-        throw new Error(`Bad date on article: ${file.path}`)
-      } else if (!valid(metaDate)) {
-        throw new Error(`Bad date in article frontmatter: ${file.path}`)
-      } else if (filenameDate.getTime() !== metaDate.getTime()) {
+    if (file.path.includes(ARTICLES_CONTENT_DIR)) {
+      const filenameDate = file.stem.split('-').slice(0, 3).join('-')
+      const metaDate = file.data.astro.frontmatter.pubDate
+      if (filenameDate !== metaDate) {
         throw new Error(`Filename doesn't match frontmatter date: ${file.path}`)
       }
     }
