51ea1a6820f478dcc8724605ea42b16e472fac0b
nguyenvukhang <brew4k@gmail.com>
2026-02-20 10:42:57 +0800

    Remove all typescript

---
+++ src/components/icons.jsx
@@ -0,0 +1,14 @@
+export const Logo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 13 17"
+    fill="none"
+    {...props}
+  >
+    <path
+      d="M1 11C8 6.5 9 1 7 1C3 1 4.5 8 4 16C4.5 10.5 7.5 7 10 7C12 7 8 14 4 13C7.5 13.5 7.5 15.5 11 16"
+      stroke-linecap="round"
+      stroke-width="1"
+    ></path>
+  </svg>
+)
