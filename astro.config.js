2525fdfecfb089ff6e003f0d75e9c9533d31909c
nguyenvukhang <brew4k@gmail.com>
2026-04-14 18:47:13 +0800

    Remove mdx filename check

--- astro.config.js
+++ astro.config.js
@@ -4,23 +4,6 @@ import solid from "@astrojs/solid-js"
 import tailwindcss from "@tailwindcss/vite"
 import githubLight from "./src/styles/github-light.json"
 
-function mdxCheckFilenameDate() {
-  /**
-   * @import {Root} from "mdast"
-   * @import { VFile } from "vfile"
-   * @param {Root} _tree
-   * @param {VFile} file
-   */
-  return function (_tree, file) {
-    const pubDate = file.data?.astro?.frontmatter?.pubDate
-    if (pubDate === undefined || pubDate === null) return
-
-    if (!file.basename.startsWith(pubDate)) {
-      throw new Error(`Filename doesn't match frontmatter date: ${file.path}`)
-    }
-  }
-}
-
 // has to match tailwind's "bg-fg4".
 githubLight.colors["editor.background"] = "var(--color-fg4)"
 
@@ -28,7 +11,7 @@ githubLight.colors["editor.background"] = "var(--color-fg4)"
 export default defineConfig({
   server: { port: 3000 },
   markdown: {
-    remarkPlugins: [mdxCheckFilenameDate],
+    remarkPlugins: [],
     shikiConfig: { theme: githubLight, wrap: false },
   },
   vite: {
